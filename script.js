a5c8b09eaf7ff72b0c26d125e5066decd22c34fc
Cbach <ceisenbach@codys-mbp.home>
2019-11-16 02:46:09 -0600

    City & Weather Display

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var button = $("#button-addon1");
 
 
-
 $(button).on("click", function () {
     // In this case, the "this" keyword refers to the button that was clicked
     var zip = $.trim($(".form-control").val());
     console.log(zip)
     // Constructing a URL to search Giphy for the name of the person who said the quote
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&APPID=8b56516a3d32f8be50c3c2b2e0638ee8";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&units=imperial&APPID=8b56516a3d32f8be50c3c2b2e0638ee8";
 
     // Performing our AJAX GET request
     $.ajax({
@@ -16,6 +15,11 @@ $(button).on("click", function () {
     })
         // After the data comes back from the API
         .then(function (response) {
-        console.log(response)
+            var cityName = response.name;
+        console.log(response);
+        localStorage.setItem("search-history", response.name);
+        localStorage.getItem("search-history")
+        $("#city-name").text(response.name);
+        $("#city-temp").text("Temperature: " + response.main.temp + " °F");
         });
     });
